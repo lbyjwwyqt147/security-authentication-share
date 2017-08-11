c8db0e926911afc4b44d7d61efbc713f9852cc18
liujunyi <532273386@qq.com>
2017-08-11 17:48:17 +0800

    xx

--- src/main/java/pers/ljy/background/share/result/ApiResultCode.java
+++ src/main/java/pers/ljy/background/share/result/ApiResultCode.java
@@ -8,7 +8,7 @@ package pers.ljy.background.share.result;
 public enum ApiResultCode {
 
 
-	    SUCCESS("成功.", 0), FAIL("数据操作失败.", 1),ISEXTIS("数据值重复.",1001),PARAMS("参数错误.",1002),ERROR("程序运行中发生异常.",2);
+	    SUCCESS("成功.", 0), FAIL("数据操作失败.", 1),ISEXTIS("数据值重复.",1001),PARAMS("参数错误.",1002),ERROR("程序运行中发生异常.",2),AUTHENTICATIONFALL("无权限访问.",10);
     
 	    private String msg ;
 	    private int code ;
--- src/main/java/pers/ljy/background/share/result/BaseApiResultView.java
+++ src/main/java/pers/ljy/background/share/result/BaseApiResultView.java
@@ -9,6 +9,7 @@ public  class BaseApiResultView {
 	public static final BaseApiResultView SUCCESS = new BaseApiResultView(0, "成功.");
 	public static final BaseApiResultView FAIL = new BaseApiResultView(1, "数据操作失败.");
 	public static final BaseApiResultView ERROR = new BaseApiResultView(2, "程序运行中发生异常.");
+	public static final BaseApiResultView AUTHENTICATIONFALL = new BaseApiResultView(10, "无权限访问.");
     /* 代码 */
 	private int status;
 	/* 消息 */
